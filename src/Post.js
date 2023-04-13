96c6f647f688bf3e2883bc171d877cef6fa6f291
DLong <daveallenlong@gmail.com>
2023-04-13 11:12:46 +0900

    First commit

---
+++ src/Post.js
@@ -0,0 +1,25 @@
+export default function Post() {
+	return (
+		<div className="post">
+			<div className="image">
+				<img
+					src="https://img.redbull.com/images/c_fill,w_1200,h_630,g_auto,f_auto,q_auto/redbullcom/2012/10/17/1331578129949_1/damon-hill-driving-for-williams-back-in-1996"
+					alt="Williams FW18 - Hill"
+				/>
+			</div>
+			<div className="texts">
+				<h2>Damon Hill</h2>
+				<p className="info">
+					<a href="/" className="author">
+						Dave Long
+					</a>
+					<time>12-04-2023 10:34</time>
+				</p>
+				<p className="summary">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+					soluta quo accusantium blanditiis, laborum voluptas amet ab numquam!
+				</p>
+			</div>
+		</div>
+	);
+}
---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function RegisterPage() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const register = async (e) => {
+		e.preventDefault();
+
+		await fetch("http://localhost:4000/register", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ username, password }),
+		});
+	};
+
+	console.log(username, password);
+	return (
+		<form className="register" onSubmit={register}>
+			<h1>Register</h1>
+			<input
+				type="text"
+				placeholder="username"
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+			/>
+			<input
+				type="password"
+				placeholder="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button>Register</button>
+		</form>
+	);
+}
