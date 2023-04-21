ef687a25f76367266c71abd23c27a6121debfae7
DLong <daveallenlong@gmail.com>
2023-04-21 12:15:06 +0900

    added - EditPost page

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,26 +1,38 @@
-import { useState } from "react";
-import { Navigate } from "react-router";
-import "react-quill/dist/quill.snow.css";
+import { useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router";
 import Editor from "../Editor";
 
-export default function CreatePost() {
+export default function EditPost() {
+	const { id } = useParams();
 	const [title, setTitle] = useState("");
 	const [summary, setSummary] = useState("");
 	const [content, setContent] = useState("");
 	const [files, setFiles] = useState("");
 	const [redirect, setRedirect] = useState(false);
 
-	const createNewPost = async (e) => {
-		e.preventDefault();
+	useEffect(() => {
+		fetch(`http://localhost:4000/post/${id}`).then((response) => {
+			response.json().then((postInfo) => {
+				setTitle(postInfo.title);
+				setContent(postInfo.content);
+				setSummary(postInfo.summary);
+			});
+		});
+	}, [id]);
 
+	const updatePost = async (e) => {
+		e.preventDefault();
 		const data = new FormData();
 		data.set("title", title);
 		data.set("summary", summary);
 		data.set("content", content);
-		data.set("file", files[0]);
+		data.set("id", id);
+		if (files?.[0]) {
+			data.set("file", files?.[0]);
+		}
 
 		const response = await fetch("http://localhost:4000/post", {
-			method: "POST",
+			method: "PUT",
 			body: data,
 			credentials: "include",
 		});
@@ -30,10 +42,11 @@ export default function CreatePost() {
 	};
 
 	if (redirect) {
-		return <Navigate to={"/"} />;
+		return <Navigate to={`/post/${id}`} />;
 	}
+
 	return (
-		<form onSubmit={createNewPost}>
+		<form onSubmit={updatePost}>
 			<input
 				type="title"
 				placeholder="title"
@@ -47,8 +60,8 @@ export default function CreatePost() {
 				onChange={(e) => setSummary(e.target.value)}
 			/>
 			<input type="file" onChange={(e) => setFiles(e.target.files)} />
-			<Editor value={content} onChange={setContent} />
-			<button style={{ marginTop: "10px" }}>Create Post</button>
+			<Editor onChange={setContent} value={content} />
+			<button style={{ marginTop: "10px" }}>Update Post</button>
 		</form>
 	);
 }
